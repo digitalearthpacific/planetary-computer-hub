118cd76ae921c7609f6407db7458c8c560c90bdc
Alex Leith <alexgleith@gmail.com>
2023-08-22 11:24:18 +1200

    Rename grafana to monitoring

--- terraform/resources/monitoring.tf
+++ terraform/resources/monitoring.tf
@@ -1,6 +1,6 @@
-resource "kubernetes_namespace" "grafana" {
+resource "kubernetes_namespace" "monitoring" {
   metadata {
-    name = "grafana"
+    name = "monitoring"
   }
 }
 
@@ -8,7 +8,7 @@ resource "kubernetes_secret" "grafana_db_credentials" {
 
   metadata {
     name      = "grafana-admin-secret"
-    namespace = kubernetes_namespace.grafana.metadata[0].name
+    namespace = kubernetes_namespace.monitoring.metadata[0].name
   }
   data = {
     admin-user     = split(":", data.azurerm_key_vault_secret.grafana_admin_secret.value)[0]
@@ -34,7 +34,7 @@ data "template_file" "grafana" {
 resource "kubernetes_secret" "grafana" {
   metadata {
     name      = "grafana-values"
-    namespace = kubernetes_namespace.grafana.metadata[0].name
+    namespace = kubernetes_namespace.monitoring.metadata[0].name
   }
 
   data = {
