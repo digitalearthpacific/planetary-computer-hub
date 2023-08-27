a018ae9acb6aece24208c4db7bc54d31eb9aa7e5
Alex Leith <alexgleith@gmail.com>
2023-08-28 09:50:02 +1000

    Add DNS to Terraform

---
+++ terraform/dep-staging/dns.tf
@@ -0,0 +1,26 @@
+provider "azurerm" {
+  features {}
+}
+
+locals {
+  resource_group = "dep-staging-rg"
+  traefik_ip_address = "51.124.54.190"
+  applications = ["argo", "grafana", "titiler", "stac"]
+}
+
+# Load the existing Azure DNS zone
+data "azurerm_dns_zone" "staging" {
+  name                = "staging.digitalearthpacific.org"
+  resource_group_name = local.resource_group
+}
+
+# Create a DNS record for each application
+resource "azurerm_dns_a_record" "applications" {
+  for_each = toset(local.applications)
+
+  name                = each.key
+  zone_name           = data.azurerm_dns_zone.staging.name
+  resource_group_name = local.resource_group
+  ttl                 = 300
+  records             = [local.traefik_ip_address]
+}
